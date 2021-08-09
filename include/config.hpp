b4c2016b544953e51a1c904265f5a28bae61ec4f
Dschahannam <uwunoozles@gmail.com>
2021-08-09 17:02:19 +0200

    extend skeleton config

--- include/config.hpp
+++ include/config.hpp
@@ -10,6 +10,34 @@ public:
         struct Players {
             bool enabled = true;
             bool name = false;
+            bool skeleton = false;
+
+			struct Skeletons {
+				bool HEAD = true;
+				bool NECK = true;
+				bool RIGHT_HAND = true;
+				bool RIGHT_FOREARM = true;
+				bool RIGHT_UPPER_ARM = true;
+				bool RIGHT_CLAVICLE = true;
+				bool LEFT_HAND = true;
+				bool LEFT_FOREARM = true;
+				bool LEFT_UPPER_ARM = true;
+				bool LEFT_CLAVICLE = true;
+				bool PELVIS = true;
+				bool SPINE_ROOT = true;
+				bool SPINE0 = true;
+				bool SPINE1 = true;
+				bool SPINE2 = true;
+				bool SPINE3 = true;
+				bool RIGHT_TOE = true;
+				bool RIGHT_FOOT = true;
+				bool RIGHT_CALF = true;
+				bool RIGHT_THIGH = true;
+				bool LEFT_TOE = true;
+				bool LEFT_FOOT = true;
+				bool LEFT_CALF = true;
+				bool LEFT_THIGH = true;
+			} active_skeletons;
         } player;
     } visuals;
 };
